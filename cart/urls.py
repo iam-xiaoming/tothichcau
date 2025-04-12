130dce5ed833355ce05af085f7d7b4257f66a00f
Minh <minhnguyen47431@gmail.com>
2025-04-12 15:50:44 +0700

    fix cart

--- cart/urls.py
+++ cart/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('cart/', views.cart_view, name='cart'), 
     path("checkout/", views.CheckoutView.as_view(), name="checkout"),
     path('create-checkout-session/', views.CreateCheckoutSessionView.as_view(), name='create_checkout_session'),
     path("success/", views.success, name="success"),
