a3fac27862d71b2a7a3d963d7c65595568364c91
TranHuyen <tranthihuyenaaaaa@gmail.com>
2025-04-12 23:11:28 +0700

    Add media

--- GameArt/urls.py
+++ GameArt/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import homepage.urls
 import games.urls
 import thanks.urls
@@ -30,3 +32,5 @@ urlpatterns = [
     path('', include(users.urls)),
     path('', include(cart.urls))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- games/models.py
+++ games/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from users.models import MyUser
+import stripe
+from django.conf import settings
+from decimal import Decimal
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 # Create your models here.
 
@@ -18,20 +23,48 @@ from users.models import MyUser
 # - quantity: interger
 
 class Game(models.Model):
-    id = models.CharField(max_length=100, primary_key=True)
-    price_id = models.CharField(max_length=255, unique=True)
-    title = models.CharField(max_length=255)
+    stripe_product_id = models.CharField(max_length=100, blank=True, null=True, editable=False)
+    stripe_price_id = models.CharField(max_length=100, blank=True, null=True, editable=False)
+    
+    name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     publisher = models.CharField(max_length=255)
     release_date = models.DateTimeField(default=timezone.now)
-    image_url = models.URLField()
+    image = models.ImageField(upload_to='game_images', default='game_images/default.png')
     category = models.CharField(max_length=255)
     quantity = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.title
+        return self.name
+
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        try:
+            # Create Stripe product
+            stripe_product = stripe.Product.create(
+                name=self.name,
+                description=self.description
+            )
+
+            # Calculate discounted price
+            discounted_price = self.price * (Decimal('1.00') - (self.discount / Decimal('100.00')))
+
+            # Create Stripe price
+            stripe_price = stripe.Price.create(
+                product=stripe_product.id,
+                unit_amount=int(discounted_price * 100),
+                currency='usd',
+            )
+
+            self.stripe_product_id = stripe_product.id
+            self.stripe_price_id = stripe_price.id
+            super().save(update_fields=['stripe_product_id', 'stripe_price_id'])
+        except Exception as e:
+            print(f"Stripe error: {e}")
+            raise
     
     
     
