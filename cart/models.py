0875fd762465d18b942c522e726e8376c5bbad32
Minh <minhnguyen47431@gmail.com>
2025-04-14 12:07:23 +0700

    add cart feature

--- cart/models.py
+++ cart/models.py
@@ -14,14 +14,24 @@ class Order(models.Model):
     
 class Transaction(models.Model):
     STATUS_CHOICES = [
-        ('complete', 'Complete'),
+        ('completed', 'Completed'),
         ('failed', 'Failed')
     ]
     
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='transactions')
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='transactions')
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='transactions')
+    
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='failed')
     created_at = models.DateTimeField(auto_now_add=True)
+    session_id = models.CharField(max_length=255)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    customer_email = models.EmailField()
+    brand = models.CharField(max_length=255)
+    last4 = models.PositiveIntegerField()
+    phone = models.CharField(max_length=255, blank=True, null=True)
+    exp_month = models.PositiveIntegerField()
+    exp_year = models.PositiveIntegerField()
 
     def __str__(self):
         return f"Transaction #{self.id} - {self.status.capitalize()}"
