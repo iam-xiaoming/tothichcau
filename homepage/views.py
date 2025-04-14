47faa1d427bf3b272ad97bd1c3364e9ea1f61183
TranHuyen <tranthihuyenaaaaa@gmail.com>
2025-04-14 18:46:42 +0700

    fix home page

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from games.models import Game
+from games.models import Game, GameHero
 from django.views.generic import ListView
 
 # Create your views here.
@@ -7,3 +7,8 @@ class ListGameView(ListView):
     model = Game
     template_name = 'homepage/home.html'
     context_object_name = 'games'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['gameheros'] = GameHero.objects.all()
+        return context
