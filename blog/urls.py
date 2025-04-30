0ddecc0bcba92a5ca84b949744abfdf1b8114e31
Huyen bo lao <tranthihuyenaaaaa@gmail.com>
2025-04-30 20:24:43 +0700

    add fix blog html, fix css

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog', views.blog, name='blog'),
-    path('blog-detail', views.blog_detail, name='blog-detail')
+    path('blog/', views.blog, name='blog'),
+    path('blog-detail/', views.blog_detail, name='blog-detail'),
+    path('create-post/', views.create_blog, name='create-post')
 ]
