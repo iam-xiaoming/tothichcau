96dad2303a5f0f17ae62615966d451cf811f6b12
Huyen bo lao <tranthihuyenaaaaa@gmail.com>
2025-05-01 08:57:47 +0700

    fix html

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import MyUser
+from game_features.models import Category
 
 # Create your models here.
+# class Post(models.Model):
+#     user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='posts')
+#     categories = models.ManyToManyField(Category, related_name='posts')
+#     title = models.CharField(max_length=128)
+#     excerpt = models.TextField(max_length=255)
+#     content = 
