dfde49278bd9b13d32494b53381ce8cfeb7dbe4c
Huyen bo lao <tranthihuyenaaaaa@gmail.com>
2025-05-01 10:52:12 +0700

    add post form

--- blog/models.py
+++ blog/models.py
@@ -3,9 +3,20 @@ from users.models import MyUser
 from game_features.models import Category
 
 # Create your models here.
-# class Post(models.Model):
-#     user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='posts')
-#     categories = models.ManyToManyField(Category, related_name='posts')
-#     title = models.CharField(max_length=128)
-#     excerpt = models.TextField(max_length=255)
-#     content = 
+class Tag(models.Model):
+    name = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='posts')
+    categories = models.ManyToManyField(Category, related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts')
+    title = models.CharField(max_length=128)
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
