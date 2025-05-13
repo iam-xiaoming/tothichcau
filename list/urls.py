2d39ced3f24d91040fd8c6badad56f0bc35d0830
Huyen bo lao <tranthihuyenaaaaa@gmail.com>
2025-05-13 09:39:30 +0700

    add features

--- list/urls.py
+++ list/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('game-list/', views.GameListView.as_view(), name='game-list'),
     path('category-list/<str:slug>/', views.category_view, name='category-list'),
-    path('action/list/<str:action>/', views.action_view, name='action-list')
+    path('action/list/<str:action>/', views.action_view, name='action-list') # trending / sale
 ]
