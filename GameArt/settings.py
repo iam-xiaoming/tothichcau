64ebc9af6c9af4fe805f4ff2b7dca6e90fc44c4b
Minh <minhnguyen47431@gmail.com>
2025-05-15 14:53:26 +0700

    fix

--- GameArt/settings.py
+++ GameArt/settings.py
@@ -89,8 +89,8 @@ LOGGING = {
 
 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-# ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 print(config('DB_NAME'))
 
