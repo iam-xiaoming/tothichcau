82d2ce0901030bacc9e62a4b12f96095c42bb747
Minh <minhnguyen47431@gmail.com>
2025-05-25 20:15:09 +0700

    add search dlcs

--- games/documents.py
+++ games/documents.py
@@ -1,6 +1,6 @@
 from django_elasticsearch_dsl import Document, fields
 from django_elasticsearch_dsl.registries import registry
-from games.models import Game, Category
+from games.models import Game, Category, DLC
 
 @registry.register_document
 class GameDocument(Document):
@@ -50,3 +50,54 @@ class GameDocument(Document):
     def get_instances_from_related(self, related_instance):
         if isinstance(related_instance, Category):
             return related_instance.game_set.all()
+        
+        
+
+@registry.register_document
+class DLCDocument(Document):
+    discounted_price = fields.FloatField(attr='discounted_price')
+    categories = fields.NestedField(properties={
+        'id': fields.IntegerField(),
+        'name': fields.TextField(analyzer='custom_edge_ngram'),
+    })
+    
+    class Index:
+        name = 'dlcs'
+        settings = {
+            "number_of_shards": 1,
+            "number_of_replicas": 0,
+            "analysis": {
+                "analyzer": {
+                    "custom_edge_ngram": {
+                        "type": "custom",
+                        "tokenizer": "standard",
+                        "filter": ["lowercase", "edge_ngram_filter"]
+                    }
+                },
+                "filter": {
+                    "edge_ngram_filter": {
+                        "type": "edge_ngram",
+                        "min_gram": 2,
+                        "max_gram": 20
+                    }
+                }
+            }
+        }
+        
+    class Django:
+        model = DLC
+        fields = ['name', 'image']
+        related_models = [Category]
+    
+    def prepare_categories(self, instance):
+        """
+        Prepare the categories field by extracting id and name from related Category objects.
+        """
+        return [
+            {'id': category.id, 'name': category.name}
+            for category in instance.categories.all()
+        ]
+        
+    def get_instances_from_related(self, related_instance):
+        if isinstance(related_instance, Category):
+            return related_instance.dlc_set.all()
--- search/views.py
+++ search/views.py
@@ -4,9 +4,9 @@ from rest_framework.response import Response as DRFResponse
 from rest_framework import status
 from elasticsearch_dsl import Q
 from search.serializers import SearchQuerySerializer
-from games.documents import GameDocument
-from games.serializers import GameSerializer
-from games.models import Game
+from games.documents import GameDocument, DLCDocument
+from games.serializers import GameSerializer, DLCSerializer
+from games.models import Game, DLC
 from users.models import UserGame
 from users.documents import UserGameDocument
 from users.serializers import UserGameSerializer
@@ -14,14 +14,19 @@ from users.documents import UserGameDocument
 
 # Create your views here.
 class GameSearchAPIView(APIView):
-    serializer_class = GameSerializer
-    document_class = GameDocument
+    # define document and serializer
+    game_serializer_class = GameSerializer
+    dlc_serializer_class = DLCSerializer
+    game_document_class = GameDocument
+    dlc_document_class = DLCDocument
     query_serializer_class = SearchQuerySerializer
 
     def elasticsearch_query_expression(self, query):
         return Q(
             "bool",
             should=[
+                # search base on 3 methods
+                # match name with boost = 2
                 Q('multi_match', query=query, fields=['name^2', 'categories.name'], fuzziness='AUTO'),
                 Q("match_phrase_prefix", name={"query": query, "boost": 3}),
                 Q(
@@ -33,7 +38,7 @@ class GameSearchAPIView(APIView):
                     )
                 ),
             ],
-            # just need to match one in both of them
+            # just need to match one in both of 3 methods
             minimum_should_match=1
         )
 
@@ -45,20 +50,38 @@ class GameSearchAPIView(APIView):
 
         query_data = search_query.data
         
-        # search
+        # searching
         try:
             q = self.elasticsearch_query_expression(query_data["query"])
-            search = self.document_class.search().query(q)
+            game_search = self.game_document_class.search().query(q)
+            dlc_search = self.dlc_document_class.search().query(q)
 
-            search = search[query_data["offset"]: query_data["offset"] + query_data["limit"]]
-            response = search.execute()
+            # searching games
+            game_search = game_search[query_data["offset"]: query_data["offset"] + query_data["limit"]]
+            game_response = game_search.execute()
+            
+            # searching dlcs
+            dlc_search = dlc_search[query_data["offset"]: query_data["offset"] + query_data["limit"]]
+            dlc_response = dlc_search.execute()
             
             # serializer results - hit is document
-            game_ids = [hit.meta.id for hit in response.hits]
+            game_ids = [hit.meta.id for hit in game_response.hits]
             games = Game.objects.filter(id__in=game_ids)
-            serializer = self.serializer_class(games, many=True)
             
-            return DRFResponse(serializer.data, status=status.HTTP_200_OK)
+            dlc_ids = [hit.meta.id for hit in dlc_response.hits]
+            dlcs = DLC.objects.filter(id__in=dlc_ids)
+            
+            game_serializer = self.game_serializer_class(games, many=True)
+            dlc_serializer = self.dlc_serializer_class(dlcs, many=True)
+            
+            games_data = [{'type': 'game', **item} for item in game_serializer.data]
+            dlcs_data = [{'type': 'dlc', **item} for item in dlc_serializer.data]
+
+            data = {
+                'items': games_data + dlcs_data
+            }
+            
+            return DRFResponse(data, status=status.HTTP_200_OK)
         
         except Exception as e:
             return DRFResponse(f"Error: {str(e)}", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
