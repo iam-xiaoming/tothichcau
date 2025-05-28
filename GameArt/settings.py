66abeb230f8c5449ed3dfd1fc593900f8334d0ce
Minh <minhnguyen47431@gmail.com>
2025-05-28 15:52:34 +0700

    fix

--- GameArt/settings.py
+++ GameArt/settings.py
@@ -128,7 +128,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [(os.getenv("REDIS_HOST", "localhost"), 6379)],
         },
     },
 }
